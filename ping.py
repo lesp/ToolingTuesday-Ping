e1e1d0cf306db57023cc9c75a33854375f909711
Les <lespounder@gmail.com>
2017-10-16 19:57:55 +0100

    removed commented out lines

--- ping.py
+++ ping.py
@@ -10,13 +10,7 @@ sleep(2)
 
 spinner.stop()
 spinner.succeed("Pinger initialisation complete")
-#spinner.info('Type pinger("IP") to start. IP can be an IP address or hostname')
-#def pinger():
-
 response = os.system("ping -c 1 " + sys.argv[1])
-#spinner.start()
-#sleep(1)
-#spinner.stop()
 if response == 0:
     print("============")
     spinner.succeed(sys.argv[1]+" is up")
